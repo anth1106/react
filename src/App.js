c9b4a43126f72ffdfc54fb667b83ed93bd49015f
Anthony Pozo <anthonypozo1@gmail.com>
2022-01-28 19:21:47 +0100

    gatisha

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Estas orgullosa gatilla? :P
+          Edit <code>src/App.js</code> gatisha :P
         </p>
         <a
           className="App-link"
